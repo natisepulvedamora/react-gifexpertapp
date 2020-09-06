4a0aa435130a3e726ada761a9d9b3cb79a9b8343
Natalia <natisepulvedamora@gmail.com>
2020-09-05 23:48:19 -0400

    docs agregado

---
+++ docs/precache-manifest.9ae7c6b707a10c40093ccda2d092c4a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736cad3cfb63e5e56f8251553942c94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9c8954b9df35b97a72",
+    "url": "/static/css/main.9cccd4ee.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9c8954b9df35b97a72",
+    "url": "/static/js/main.a3a0ae80.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
