60b8f8344d9e07051f4671d175ad83d080d219b9
Natalia <natisepulvedamora@gmail.com>
2020-09-06 01:07:24 -0300

    https add

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(categoria) => {    
         const apiKey='B0qECaENyRaEoN9VYIv4KHWOovaNfwVT';
-        const url=`http://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=${apiKey}`;
+        const url=`https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=${apiKey}`;
         const respuesta=await fetch(url);
         const {data}=await respuesta.json();
         //console.log(categoria);
